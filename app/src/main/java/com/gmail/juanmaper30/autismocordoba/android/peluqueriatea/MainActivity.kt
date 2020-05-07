2337f9c4bd71920c54d8df4d5404c4054b401654
Juanma <juanmaper30@gmail.com>
2020-05-07 19:56:49 +0200

    Añadir actividad main vacia

---
+++ app/src/main/java/com/gmail/juanmaper30/autismocordoba/android/peluqueriatea/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.juanmaper30.autismocordoba.android.peluqueriatea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
